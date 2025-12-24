122f3583145e75a459a3d73def365a49896ade50
Saintiron_hi <hid_develop_acc@neowiz.com>
2025-12-24 13:37:39 +0900

    fix(desk): disable flask reloader to reduce cpu usage

--- desk/manual_crawler.py
+++ desk/manual_crawler.py
@@ -53,4 +53,4 @@ app.register_blueprint(utility_bp)
 
 if __name__ == '__main__':
     # Port 5500 as requested
-    app.run(host='0.0.0.0', debug=True, port=5500)
+    app.run(host='0.0.0.0', debug=True, port=5500, use_reloader=False)
